3a1768b388b628871bc2c13439d71bc9ed2e470c
Danilo Carvalho <danilo.carvalho@inatel.br>
2020-04-04 15:00:32 -0300

    Second Commit

--- spring-boot-playground/playground/src/main/java/com/example/springboot/service/impl/CalcImpl.java
+++ spring-boot-playground/playground/src/main/java/com/example/springboot/service/impl/CalcImpl.java
@@ -1,13 +1,3 @@
-//**********************************************************************
-// Copyright (c) 2020 Telefonaktiebolaget LM Ericsson, Sweden.
-// All rights reserved.
-// The Copyright to the computer program(s) herein is the property of
-// Telefonaktiebolaget LM Ericsson, Sweden.
-// The program(s) may be used and/or copied with the written permission
-// from Telefonaktiebolaget LM Ericsson or in accordance with the terms
-// and conditions stipulated in the agreement/contract under which the
-// program(s) have been supplied.
-// **********************************************************************
 package com.example.springboot.service.impl;
 
 import org.springframework.stereotype.Service;
